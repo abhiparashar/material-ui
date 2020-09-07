f1b85dbbad5b4ec79444d6c3504d8776d9872f87
abhi parashar <aparashar103@gmail.com>
2020-09-07 09:07:47 +0530

    first commit

---
+++ src/pages/Employees/Employee.js
@@ -0,0 +1,10 @@
+import React from './node_modules/react'
+import EmployeeForm from './EmployeeForm'
+
+export default function Employee() {
+    return (
+        <div>
+            <EmployeeForm/>
+        </div>
+    )
+}
